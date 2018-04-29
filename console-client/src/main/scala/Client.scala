e4c44beb044760cfa09bdca7ce9ff32bdac5460a
unknown <astartes91@gmail.com>
2018-04-29 20:40:03 +0700

    TCP changes

--- console-client/src/main/scala/Client.scala
+++ console-client/src/main/scala/Client.scala
@@ -1,7 +1,7 @@
 import java.net.InetSocketAddress
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Kill}
-import akka.io.Tcp.{CommandFailed, Connect, Connected}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Kill}
+import akka.io.Tcp._
 import akka.io.{IO, Tcp}
 
 class Client(remoteAddress: InetSocketAddress, actorSystem: ActorSystem) extends Actor with ActorLogging {
@@ -13,6 +13,24 @@ class Client(remoteAddress: InetSocketAddress, actorSystem: ActorSystem) extends
       log.error(s"Failed to connect to ${remoteAddress.toString}")
       self ! Kill
       actorSystem.terminate()
-    case Connected(remote, local) => log.info(s"Successfully connected to $remote!")
+    case Connected(remote, local) =>
+      log.info(s"Successfully connected to $remote!")
+
+      val connection: ActorRef = sender()
+      connection ! Register(self)
+
+      context become {
+        case CommandFailed(w: Write) =>
+          log.error("Failed to write request.")
+        case Received(data) =>
+          log.info("Received response.")
+          println(data.decodeString("UTF-8"))
+        case "close" =>
+          log.info("Closing connection")
+          connection ! Close
+        case _: ConnectionClosed =>
+          log.info("Connection closed by server.")
+          context stop self
+      }
   }
 }
--- server/src/main/scala/TcpServer.scala
+++ server/src/main/scala/TcpServer.scala
@@ -1,18 +1,25 @@
 import java.net.InetSocketAddress
 
-import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem}
 import akka.io.Tcp._
 import akka.io.{IO, Tcp}
+import akka.util.ByteString
 
 class TcpServer(address: InetSocketAddress, actorSystem: ActorSystem) extends Actor with ActorLogging {
 
   IO(Tcp)(actorSystem) ! Bind(self, address)
 
   override def receive: Receive = {
-    case Bound(localAddress) => log.info(s"Tcp server started at $localAddress")
+    case b @ Bound(localAddress) =>
+      log.info(s"Tcp server started at $localAddress")
+      context.parent ! b
     case CommandFailed(_: Bind) =>
       log.error("Error!")
       context.stop(self)
-    case Connected(remoteAddress, localAddress) => log.info(s"Tcp client from $remoteAddress connected!")
+    case Connected(remoteAddress, localAddress) =>
+      log.info(s"Tcp client from $remoteAddress connected!")
+      val connection: ActorRef = sender()
+      connection ! Register(self)
+      connection ! Write(ByteString("Hello!"))
   }
 }
